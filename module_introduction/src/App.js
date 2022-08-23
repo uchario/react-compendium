9bf8c9a76357b0fa76b4a6fd2a6eb4e8de01a242
uchario <uchario@yahoo.com>
2022-08-23 14:29:23 +0100

    add dynamic numbering to 'id' property based on array length

--- module_introduction/src/App.js
+++ module_introduction/src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   const addGoalHandler = enteredText => {
     setCourseGoals(prevGoals => {
       const updatedGoals = [...prevGoals];
-      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
+      updatedGoals.unshift({ text: enteredText, id: 'g' + (courseGoals.length + 1) });
       return updatedGoals;
     });
   };
