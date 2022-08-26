1d580a3324bbbfd7439ff04bea3a81085eee4216
uchario <uchario@yahoo.com>
2022-08-26 08:37:12 +0100

    implement addUserHandler, add UserInput & UserList components

--- test_project_two/src/App.js
+++ test_project_two/src/App.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 
+import UserInput from './components/UserInput/UserInput';
+import UserList from './components/UserList/UserList';
+
+const USER_DETAILS = [];
 
 function App() {
+  const [userDetails, setUserDetails] = useState(USER_DETAILS);
+
+  const addUserHandler = (user) => {
+    setUserDetails((prevDetails) => {
+      const updateUserDetails = [...prevDetails];
+      updateUserDetails.unshift(user)
+      return updateUserDetails;
+    });
+  };
+
+  let content = (
+    <p>No user entry...</p>
+  );
+
+  if (userDetails.length > 0) {
+    content = (
+      <UserList
+        users={userDetails}
+      />
+    );
+  };
+
   return (
     <div>
-
+      <section>
+      <UserInput 
+        onAddUser={addUserHandler}
+      />
+      </section>
+      
+      <section>
+        {content}
+      </section>
     </div>
   );
 }
