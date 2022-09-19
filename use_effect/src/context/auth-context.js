70cabc1d60bf2b83310c592d95124d00c0f57b32
uchario <uchario@yahoo.com>
2022-09-19 23:11:50 +0100

    Add React, other hooks, AuthContext and AuthContextProvider

--- use_effect/src/context/auth-context.js
+++ use_effect/src/context/auth-context.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 
-const AuthContext = React.createContext({
+const AuthContext = createContext({
     isLoggedIn: false,
     onLogout: () => {},
     onLogin: () => {}
