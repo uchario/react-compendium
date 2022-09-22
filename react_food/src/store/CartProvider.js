4d18ab9212724ec1259ae98934c540d883feeb78
uchario <uchario@yahoo.com>
2022-09-22 16:32:27 +0100

    Add CartProvider.js

---
+++ react_food/src/store/CartProvider.js
@@ -0,0 +1,26 @@
+import CartContext from "./cart-context";
+
+const CartProvider = (props) => {
+    const addItemToCartHandler = (props) => {
+
+    };
+
+    const removeITemFromCartHandler = (props) => {
+
+    };
+
+    const cartContext = {
+        items: [],
+        totalAmount: 0,
+        addItem: addItemToCartHandler,
+        removeItem: removeITemFromCartHandler,
+    };
+
+    return (
+        <CartContext.Provider value={cartContext}>
+            {props.children}
+        </CartContext.Provider>
+    );
+};
+
+export default CartProvider;
