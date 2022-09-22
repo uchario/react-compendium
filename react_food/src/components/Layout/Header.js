a645541b38b6d8f974deef9159cf5cfd14aaf632
uchario <uchario@yahoo.com>
2022-09-22 11:32:55 +0100

    Iniital commit

---
+++ react_food/src/components/Layout/Header.js
@@ -0,0 +1,24 @@
+import React, { Fragment } from 'react';
+
+import HeaderCartButton from './HeaderCartButton';
+
+import classes from './Header.module.css';
+
+import mealsImage from '../../assets/meals.jpg';
+
+const Header = (props) => {
+    return (
+        <Fragment>
+            <header className={`${classes.header}`}>
+                <h1>React Meals</h1>
+                <HeaderCartButton/>
+            </header>
+            <div className={`${classes['main-image']}`}>
+                <img src={mealsImage} alt="img"/>
+            </div>
+        </Fragment>
+        
+    );
+}
+
+export default Header;
