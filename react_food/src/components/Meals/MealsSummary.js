ec6a0a567f3c743c1e48e97eeef1e13d657b308a
uchario <uchario@yahoo.com>
2022-09-22 15:59:47 +0100

    Add MealsSummary.js

--- react_food/src/components/Meals/MealsSummary.js
+++ react_food/src/components/Meals/MealsSummary.js
@@ -12,7 +12,7 @@ const MealsSummary = (props) => {
                 All our meals are cooked with high-quality ingredients, just-in-time and
                 of course by experienced chefs!
             </p>
-        </section>
+        </section>  
     );
 }   
 
